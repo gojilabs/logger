c41d02a24d3319d3238d6f95c29d13e8140240cd
Adam Sumner <adamsumner@gmail.com>
2017-08-12 18:18:18 -0700

    prepend timestamp only in development and test, assume production and staging log handlers add timestamps

--- logger.go
+++ logger.go
@@ -1,8 +1,12 @@
 package logger
 
 import (
-  "log"
-  "strings"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gojilabs/environment"
 )
 
 const debug = 0
@@ -16,54 +20,63 @@ const WARN = 'W'
 const ERROR = 'E'
 
 type Logger struct {
-  level int
-  msgLevel rune
+	level    int
+	msgLevel rune
+	mutex    sync.Mutex
 }
 
 func New(logLevel string) *Logger {
 
-  level := debug
-  msgLevel := DEBUG
+	level := debug
+	msgLevel := DEBUG
 
-  if logLevel != "" {
-    levelStr := strings.ToLower(logLevel)
-    if levelStr == "error" {
-      level = err
-      msgLevel = ERROR
-    } else if levelStr == "warn" {
-      level = warn
-      msgLevel = WARN
-    } else if levelStr == "info" {
-      level = info
-      msgLevel = INFO
-    }
-  }
+	if logLevel != "" {
+		levelStr := strings.ToLower(logLevel)
+		if levelStr == "error" {
+			level = err
+			msgLevel = ERROR
+		} else if levelStr == "warn" {
+			level = warn
+			msgLevel = WARN
+		} else if levelStr == "info" {
+			level = info
+			msgLevel = INFO
+		}
+	}
 
-  return &Logger{level: level, msgLevel: msgLevel}
+	return &Logger{level: level, msgLevel: msgLevel}
 }
 
 func (l *Logger) shouldWrite(level int) bool {
-  return level >= l.level
+	return level >= l.level
 }
 
 func (l *Logger) writeLine(level int, levelRune rune, msg string) {
-  if l.shouldWrite(level) {
-    log.Println(string(levelRune) + " " + msg)
-  }
+	if l.shouldWrite(level) {
+		timestamp := ""
+		if environment.Development() || environment.Test() {
+			timestamp = time.Now().Format(time.StampMicro) + " "
+		}
+
+		l.mutex.Lock()
+		defer l.mutex.Unlock()
+
+		os.Stdout.WriteString(timestamp + string(levelRune) + " " + msg + "\n")
+	}
 }
 
 func (l *Logger) Debug(msg string) {
-  l.writeLine(debug, DEBUG, msg)
+	l.writeLine(debug, DEBUG, msg)
 }
 
 func (l *Logger) Info(msg string) {
-  l.writeLine(info, INFO, msg)
+	l.writeLine(info, INFO, msg)
 }
 
 func (l *Logger) Warn(msg string) {
-  l.writeLine(warn, WARN, msg)
+	l.writeLine(warn, WARN, msg)
 }
 
 func (l *Logger) Error(msg string) {
-  l.writeLine(err, ERROR, msg)
+	l.writeLine(err, ERROR, msg)
 }
